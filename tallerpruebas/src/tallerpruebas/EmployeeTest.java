e3121ed283684232fde341822698150f433d3d3f
Renzo Pacheco <renzopachecoj@gmail.com>
2020-01-20 10:07:47 -0500

    testing

---
+++ tallerpruebas/src/tallerpruebas/EmployeeTest.java
@@ -0,0 +1,24 @@
+package tallerpruebas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeeTest {
+
+	@Test
+	void testEmployee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCalculateYearBonus() {
+		fail("Not yet implemented");
+	}
+
+}
