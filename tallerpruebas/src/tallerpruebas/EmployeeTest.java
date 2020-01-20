5ef78a93e3ccbc761eceab53e74a7746d70f91a1
Renzo Pacheco <renzopachecoj@gmail.com>
2020-01-20 10:59:58 -0500

    testing monedas

--- tallerpruebas/src/tallerpruebas/EmployeeTest.java
+++ tallerpruebas/src/tallerpruebas/EmployeeTest.java
@@ -4,21 +4,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class EmployeeTest {
+class EmployeeTest{
 
 	@Test
-	void testEmployee() {
-		fail("Not yet implemented");
+	void cambioMonedaWorker() {
+		Employee e = new Employee((float)800.00, "EURO", (float)0.05, EmployeeType.Worker);
+		assertEquals(824.33,e.cs(),0.1);
 	}
-
+	
 	@Test
-	void testCs() {
-		fail("Not yet implemented");
+	void cambioMonedaManager() {
+		Employee e = new Employee((float)2000.00, "EURO", (float)0.05, EmployeeType.Manager);
+		assertEquals(1964.35,e.cs(),0.1);
 	}
-
+	
 	@Test
-	void testCalculateYearBonus() {
-		fail("Not yet implemented");
+	void cambioMonedaWorkerSupervisor() {
+		Employee e = new Employee((float)1000.00, "EURO", (float)0.05, EmployeeType.Supervisor);
+		assertEquals(1014.36,e.cs(),0.1);
 	}
+	
 
 }
